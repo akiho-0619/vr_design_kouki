020badf6173777efbd1731935bea2d81e067b6b8
akiho-yamada <aki19.akiho@gmail.com>
2025-12-05 09:50:56 +0900

    add UI open event

---
+++ UI.js
@@ -0,0 +1,4 @@
+document.getElementById("openButton").addEventListener("click", () => {
+    console.log("openParams");
+    document.getElementById("params").style.animation = "openParams 0.5s forwards";
+});

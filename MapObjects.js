a4f76fa3b5d3e053acc5d6f852f74fc2ea14b68a
akiho-yamada <aki19.akiho@gmail.com>
2025-12-05 11:23:04 +0900

    環境関連の追加

--- MapObjects.js
+++ MapObjects.js
@@ -27,3 +27,35 @@ export class MapObject{
         }
     }
 }
+
+export class Tree extends MapObject{
+    constructor(model, position, rotation){
+        super(model, position, rotation);
+        this.type = "tree";
+        this.woodAmount = 50;
+    }
+}
+
+export class AppleTree extends MapObject{
+    constructor(model, position, rotation){
+        super(model, position, rotation);
+        this.type = "tree";
+        this.woodAmount = 50;
+    }
+}
+
+export class Rock extends MapObject{
+    constructor(model, position, rotation){
+        super(model, position, rotation);
+        this.type = "rock";
+        this.stoneAmount = 100;
+    }
+}
+
+export class River{
+    constructor(width){
+        this.width = width;
+        this.startingPoint = [Math.random()*20-10, 0, Math.random()*20-10];
+        this.endingPoint = [Math.random()*20-10, 0, Math.random()*20-10];
+    }
+}

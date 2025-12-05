123a6e94518fb10271d0ac6bb633acbee8c366d7
akiho-yamada <aki19.akiho@gmail.com>
2025-12-05 13:19:27 +0900

    changeable the wall

--- scripts.js
+++ scripts.js
@@ -3,12 +3,16 @@ const { camera, create, animate, controls, load, helper } = init("#content");
 import { Character } from "./character.js";
 // import { MapObject } from "./MapObjects.js";
 
-let map_size = [10, 10];
+const WALL_THICKNESS = 0.2;
+const INITIAL_MAP_SIZE = [10, 10];
+const INITIAL_WALL_SIZE_X = [INITIAL_MAP_SIZE[0] + WALL_THICKNESS*2, 1, WALL_THICKNESS];
+const INITIAL_WALL_SIZE_Z = [WALL_THICKNESS, 1, INITIAL_MAP_SIZE[1] + WALL_THICKNESS*2];
+
+let map_size = INITIAL_MAP_SIZE;
 let char_count = 10;
 let characters = [];
-let wall_thickness = 0.2;
-let wall_size_x = [map_size[0] + wall_thickness*2, 1, wall_thickness];
-let wall_size_z = [wall_thickness, 1, map_size[1] + wall_thickness*2];
+let wall_size_x = INITIAL_WALL_SIZE_X;
+let wall_size_z = INITIAL_WALL_SIZE_Z;
 
 controls.connect()
 camera.position.set(-2, 2, 2)
@@ -29,10 +33,10 @@ let ground = create.plane({
     }
 })
 let walls = [
-    create.cube({ size:wall_size_x, position:[0,0.5,-(map_size[1]/2 + wall_thickness/2)], option:{ color:"white"} }),
-    create.cube({ size:wall_size_x, position:[0,0.5,(map_size[1]/2 + wall_thickness/2)], option:{ color:"white"} }),
-    create.cube({ size:wall_size_z, position:[-(map_size[0]/2 + wall_thickness/2),0.5,0], option:{ color:"white"} }),
-    create.cube({ size:wall_size_z, position:[(map_size[0]/2 + wall_thickness/2),0.5,0], option:{ color:"white"} }),
+    create.cube({ size:wall_size_x, position:[0,0.5,-(map_size[1]/2 + WALL_THICKNESS/2)], option:{ color:"white"} }),
+    create.cube({ size:wall_size_x, position:[0,0.5,(map_size[1]/2 + WALL_THICKNESS/2)], option:{ color:"white"} }),
+    create.cube({ size:wall_size_z, position:[-(map_size[0]/2 + WALL_THICKNESS/2),0.5,0], option:{ color:"white"} }),
+    create.cube({ size:wall_size_z, position:[(map_size[0]/2 + WALL_THICKNESS/2),0.5,0], option:{ color:"white"} }),
 ]
 
 let stoneModel, treeModel;
@@ -72,18 +76,18 @@ document.getElementById("mapSubmit").addEventListener("click", () => {
     map_size[0] = parseInt(document.getElementById("mapSizeX").value);
     map_size[1] = parseInt(document.getElementById("mapSizeY").value);
 
-    ground.size = map_size;
+    ground.scale.set(map_size[0]/INITIAL_MAP_SIZE[0], 1, map_size[1]/INITIAL_MAP_SIZE[1]);
     console.log(map_size);
-    wall_size_x = [map_size[0] + wall_thickness*2, 1, wall_thickness];
-    wall_size_z = [wall_thickness, 1, map_size[1] + wall_thickness*2];
+    wall_size_x = [map_size[0] + WALL_THICKNESS*2, 1, WALL_THICKNESS];
+    wall_size_z = [WALL_THICKNESS, 1, map_size[1] + WALL_THICKNESS*2];
 
     for (let i = 0; i < walls.length; i++) {
         if (i < 2) {
-            walls[i].size = wall_size_x;
-            walls[i].position.z = (i === 0) ? -(map_size[1]/2 + wall_thickness/2) : (map_size[1]/2 + wall_thickness/2);
+            walls[i].scale.set(...wall_size_x.map((val, index) => val / INITIAL_WALL_SIZE_X[index]));
+            walls[i].position.z = (i === 0) ? -(map_size[1]/2 + WALL_THICKNESS/2) : (map_size[1]/2 + WALL_THICKNESS/2);
         } else {
-            walls[i].size = wall_size_z;
-            walls[i].position.x = (i === 2) ? -(map_size[0]/2 + wall_thickness/2) : (map_size[0]/2 + wall_thickness/2);
+            walls[i].scale.set(...wall_size_z.map((val, index) => val / INITIAL_WALL_SIZE_Z[index]));
+            walls[i].position.x = (i === 2) ? -(map_size[0]/2 + WALL_THICKNESS/2) : (map_size[0]/2 + WALL_THICKNESS/2);
         }
     }
 

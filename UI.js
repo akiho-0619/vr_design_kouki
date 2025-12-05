e659ec3c4c2e7fb2ad0211d0a21595c4c7feb02b
akiho-yamada <aki19.akiho@gmail.com>
2025-12-05 09:57:46 +0900

    open event

--- UI.js
+++ UI.js
@@ -1,4 +1,13 @@
 document.getElementById("openButton").addEventListener("click", () => {
-    console.log("openParams");
-    document.getElementById("params").style.animation = "openParams 0.5s forwards";
+    const button = document.getElementById("openButton");
+    if (button.textContent === "Close Model") {
+        document.getElementById("params").style.animation = "closeParams 0.5s forwards";
+        button.textContent = "Open Model";
+        return;
+    }
+    else{
+        document.getElementById("params").style.animation = "openParams 0.5s forwards";
+        button.textContent = "Close Model";
+    }
+
 });

45fb0b59fb5a247c7822db4466f298305099f151
akiho-yamada <aki19.akiho@gmail.com>
2025-12-05 10:26:03 +0900

    add parameter to character

--- character.js
+++ character.js
@@ -7,6 +7,22 @@ export class Character {
         this.position = [Math.random() * 10-5, this.size / 2, Math.random() * 10-5];
         this.body = create.cube({ size: this.size, position: this.position});
         this.MOVE_MAX_LONG = 0.2;
+
+        this.fixed_parameters = {
+            curiosity: Math.random(),   //好奇心
+            sociability: Math.random(), //社交性
+            max_energy: 100,            //最大エネルギー
+            speed: 1 + Math.random(),   //速度
+            aggression: Math.random(),  //攻撃性
+            cooperation: Math.random(), //協調性
+        }
+
+        this.variable_parameters = {
+            energy: this.fixed_parameters.max_energy, //エネルギー
+            anger: 0,                                 //怒り
+            foods:100,                                //食料
+            water:100,                                //水分
+        }
     }
     attack(target) {
         target.health -= this.strength;
